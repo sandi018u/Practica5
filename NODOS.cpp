628139b52eb28a3a3b2f11ef0b5f725751997050
sandi018u <sandi.gonzalez.ico23@gmail.com>
2019-09-13 13:32:09 -0500

    Al programa se le incluye el mismo proceso pero ahora con auxiliar y la palabra reservada new

--- NODOS.cpp
+++ NODOS.cpp
@@ -15,6 +15,9 @@ int main(int argc, char** argv){
 	struct nodo N4;
 	struct nodo N5;
 	
+	struct nodo *inicio;
+	struct nodo *aux;
+	
 	N1.valor=4;
 	N1.Ptr=NULL;
 	
@@ -36,14 +39,45 @@ int main(int argc, char** argv){
 	N4.Ptr=&N5;
 	N5.Ptr=&N1;
 	
+	cout<<" \n Usando apuntadores \n"<<endl;
 	cout<<"El valor de 2 es :"<<N1.Ptr->valor<<endl;
 	cout<<"El valor de 3 es :"<<N1.Ptr->Ptr->valor<<endl;
 	cout<<"El valor de 4 es :"<<N1.Ptr->Ptr->Ptr->valor<<endl;
 	cout<<"El valor de 5 es :"<<N1.Ptr->Ptr->Ptr->Ptr->valor<<endl;
 	cout<<"El valor de 1 es :"<<N1.Ptr->Ptr->Ptr->Ptr->Ptr->valor<<endl;
 	
+
+	aux = new nodo();
+	aux->Ptr=NULL;
+	aux->valor=4;
+	inicio=aux;
 	
+	aux=new nodo();
+	aux->Ptr=NULL;
+	aux->valor=8;
+	inicio->Ptr=aux;
 	
+	aux=new nodo();
+	aux->Ptr=NULL;
+	aux->valor=2;
+	inicio->Ptr->Ptr=aux;
+	
+	aux=new nodo();
+	aux->Ptr=NULL;
+	aux->valor=5;
+	inicio->Ptr->Ptr->Ptr=aux;
+	
+	aux=new nodo();
+	aux->Ptr=NULL;
+	aux->valor=7;
+	inicio->Ptr->Ptr->Ptr->Ptr=aux;
+	
+	cout<<" \n Usando auxiliar \n"<<endl;
+	cout<<"El valor de 1 es :"<<inicio->valor<<endl;
+	cout<<"El valor de 2 es :"<<inicio->Ptr->valor<<endl;
+	cout<<"El valor de 3 es :"<<inicio->Ptr->Ptr->valor<<endl;
+	cout<<"El valor de 4 es :"<<inicio->Ptr->Ptr->Ptr->valor<<endl;
+	cout<<"El valor de 5 es :"<<inicio->Ptr->Ptr->Ptr->Ptr->valor<<endl;
 	
 	getch();
 	return 0;
