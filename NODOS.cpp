59800206a88d89825744388196449ce3dedbc51a
sandi018u <sandi.gonzalez.ico23@gmail.com>
2019-09-13 12:41:31 -0500

    Programa para mostrar el valor de los nodos atraves de punteros

---
+++ NODOS.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+struct nodo{
+	int valor;
+	struct nodo *Ptr;
+};
+
+int main(int argc, char** argv){
+	struct nodo N1;
+	struct nodo N2;
+	struct nodo N3;
+	struct nodo N4;
+	struct nodo N5;
+	
+	N1.valor=4;
+	N1.Ptr=NULL;
+	
+	N2.valor=8;
+	N2.Ptr=NULL;
+	
+	N3.valor=2;
+	N3.Ptr=NULL;
+	
+	N4.valor=5;
+	N4.Ptr=NULL;
+	
+	N5.valor=7;
+	N5.Ptr=NULL;
+	
+	N1.Ptr=&N2;
+	N2.Ptr=&N3;
+	N3.Ptr=&N4;
+	N4.Ptr=&N5;
+	N5.Ptr=&N1;
+	
+	cout<<"El valor de 2 es :"<<N1.Ptr->valor<<endl;
+	cout<<"El valor de 3 es :"<<N1.Ptr->Ptr->valor<<endl;
+	cout<<"El valor de 4 es :"<<N1.Ptr->Ptr->Ptr->valor<<endl;
+	cout<<"El valor de 5 es :"<<N1.Ptr->Ptr->Ptr->Ptr->valor<<endl;
+	cout<<"El valor de 1 es :"<<N1.Ptr->Ptr->Ptr->Ptr->Ptr->valor<<endl;
+	
+	
+	
+	
+	getch();
+	return 0;
+}
